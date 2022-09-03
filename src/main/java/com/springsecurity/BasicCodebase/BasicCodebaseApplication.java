6ea6bc494330350aac6160e77ba00892375c0add
vinaykumarprajapati <107835976+VinayKumarPrajapati@users.noreply.github.com>
2022-09-03 11:13:31 +0530

    setup project for spring security

---
+++ src/main/java/com/springsecurity/BasicCodebase/BasicCodebaseApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.BasicCodebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCodebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCodebaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecurity/BasicCodebase/BasicCodebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.BasicCodebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCodebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
